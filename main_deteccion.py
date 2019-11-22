8309642dca0bbcdaf60451e3cdaa93f9a9404f77
Esteban Villalobos <egvillalobos@uc.cl>
2019-11-22 17:07:47 -0300

    Se crean archivos de base para detectores

---
+++ main_deteccion.py
@@ -0,0 +1,13 @@
+"""
+Template para tomar un algoritmo de detección, procesar un video y crear las carpetas
+correspondientes.
+
+Acciones:
+    - [TODO] Cargar video
+    - [TODO] Recorrer frame por frame
+    - [TODO] Posibilidad de cambiar funcion que entregue los bounding boxes
+    - [TODO] Guardar bounding boxes segun método (en su carpeta correspondiente)
+"""
+# Importar metodos de deteccion
+import dlib_detector
+import retinaface_detector
---
+++ retinaface_detector.py
@@ -0,0 +1,18 @@
+"""
+IMPLEMENTAR
+"""
+
+
+def get_boxes(imagen):
+    """
+    Recibe imagen en BRG y usando RetinaFace encuentra los bounding boxes
+
+    Debe retornar una matriz donde las filas sean los bounding boxes de
+    las caras, y las columnas sean:
+        - Coordenada x de esquina sup izquierda
+        - Coordenada y de esquina sup izquierda,
+        - Ancho
+        - Alto
+    (en este orden)
+    """
+    pass
